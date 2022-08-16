09261fa77e88ee3b25a7cbd7ef526045b51175c2
epitaxy <jaeyeol@gmail.com>
2022-08-16 01:39:14 +0000

    First Commit

--- base.py
+++ base.py
@@ -1,10 +1,11 @@
 from operator import itemgetter
 
 
-# convertion of Json Data to list
+# convertion of Json Data to list by selecting columns
 state, time = list( zip(*[ (x['state'], x['last_updated']) for x in raw_data]))
 
 # another converion thorugh itemgetter (a little faster)
 state, time = list( zip( *map(itemgetter('state', 'last_updated'), raw_data) ))
 
+# each 
 state = list(map(itemgetter('state'), raw_data))
